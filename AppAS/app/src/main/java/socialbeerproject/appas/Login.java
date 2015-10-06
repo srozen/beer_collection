3fa1372e8ebcf1cfc1749721a4e2bdec49e28b5f
Willfrit <remy-voet@outlook.fr>
2015-10-06 21:01:55 +0200

    XML Login et Inscription

---
+++ AppAS/app/src/main/java/socialbeerproject/appas/Login.java
@@ -0,0 +1,48 @@
+package socialbeerproject.appas;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends Activity implements View.OnClickListener{
+
+    private Button btnAInsc = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnAInsc = (Button) findViewById(R.id.btn_A_Inscription);
+        btnAInsc.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_A_Inscription:
+                Intent i= new Intent(this,Inscription.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
