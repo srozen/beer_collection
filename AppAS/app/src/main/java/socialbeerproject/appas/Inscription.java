396db2f4afe65ab6d295c86a2a4be7a03da5cadc
Willfrit <remy-voet@outlook.fr>
2015-10-06 20:58:49 +0200

    XML Login et Inscription

--- AppAS/app/src/main/java/socialbeerproject/appas/Inscription.java
+++ AppAS/app/src/main/java/socialbeerproject/appas/Inscription.java
@@ -1,37 +1,47 @@
 package socialbeerproject.appas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Inscription extends Activity {
+public class Inscription extends Activity implements View.OnClickListener{
+
+    private Button btnAConn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+        btnAConn = (Button) findViewById(R.id.btn_A_SeConnecter);
+        btnAConn.setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_inscription, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_A_SeConnecter:
+                Intent i= new Intent(this,Login.class);
+                startActivity(i);
+                break;
+        }
     }
 }
